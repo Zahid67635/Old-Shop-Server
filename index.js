3066856a5bc28e122091c9636315002b2762e677
Zahid <zahid67635gmail.com>
2022-11-29 01:00:15 +0600

    all products and cats api added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
@@ -8,6 +9,33 @@ require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gtp2h2a.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const allProductCollection = client.db('old-shop').collection('allProducts')
+        const allCategories = client.db('old-shop').collection('Categories')
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const products = await allProductCollection.find(query).toArray();
+            res.send(products);
+        });
+        app.get('/categories', async (req, res) => {
+            const query = {};
+            const categories = await allCategories.find(query).toArray();
+            res.send(categories);
+        });
+
+    }
+    finally {
+
+    }
+}
+run().catch(er => console.log(er))
 
 
 app.get('/', (req, res) => {
