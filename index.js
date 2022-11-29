de26d297215667a17c3ce3a5017bc636711358bf
Zahid <zahid67635gmail.com>
2022-11-29 21:55:10 +0600

    added jwt

--- index.js
+++ index.js
@@ -12,13 +12,34 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gtp2h2a.mongodb.net/?retryWrites=true&w=majority`;
-
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+//jwt verify function
+function verifyJWT(req, res, next) {
+
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send('unauthorized access');
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+
+}
 async function run() {
     try {
-        const allProductCollection = client.db('old-shop').collection('allProducts')
-        const allCategories = client.db('old-shop').collection('Categories')
+        const allProductCollection = client.db('old-shop').collection('allProducts');
+        const allCategories = client.db('old-shop').collection('Categories');
+        const bookingsCollection = client.db('old-shop').collection('bookings');
+        const usersCollection = client.db('old-shop').collection('users');
         app.get('/products', async (req, res) => {
             const query = {};
             const products = await allProductCollection.find(query).toArray();
@@ -36,6 +57,34 @@ async function run() {
             const categoryProducts = await allProductCollection.find(query).toArray();
             res.send(categoryProducts)
         })
+        app.get('/bookings', async (req, res) => {
+            const email = req.query.email;
+            const query = { email };
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings);
+        })
+
+        app.post('/bookings', async (req, res) => {
+            const bookings = req.body;
+            const result = await bookingsCollection.insertOne(bookings);
+            res.send(result);
+        })
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
+
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1d' })
+                return res.send({ accessToken: token });
+            }
+            res.status(403).send({ accessToken: '' })
+        });
 
     }
     finally {
